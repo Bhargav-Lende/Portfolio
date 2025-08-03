1ec66738cf3c1a4c34c0e8274a70a150449f9e39
Bhargav Lende <lendebj@rknec.edu>
2025-08-04 02:04:36 +0530

    c_3 : hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      
+      <img src={profile_img} alt="" />
+      <h1><span>I'm Bhargav Lende,</span> Data Scientist based in India</h1>
+      <p>I am an undergard student pursuing BTech in the field of Computer Science and Engineering with specialzation in Data Science</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
     </div>
   )
 }
