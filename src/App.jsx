105d25170ee6fe5c1320ac481edc6d190b6c1702
Bhargav Lende <lendebj@rknec.edu>
2025-08-04 23:03:56 +0530

    c_5 : Certifications Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Certifications from './Components/Certifications/Certifications'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <About/>
+      <Certifications/>
     </div>
   )
 }
