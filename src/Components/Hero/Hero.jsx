c6c64728c880eedfada370f6f4bad7cd2a7a2558
Bhargav Lende <lendebj@rknec.edu>
2025-08-06 23:51:55 +0530

    First commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt="" />
-      <h1><span>I'm Bhargav Lende,</span> Data Scientist based in Nagpur India</h1>
+      <h1><span>I'm Bhargav Lende,</span> Data Scientist based in Nagpur Maharashtra India</h1>
       <p>I am an undergard student pursuing BTech in the field of Computer Science and Engineering with specialzation in Data Science</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
